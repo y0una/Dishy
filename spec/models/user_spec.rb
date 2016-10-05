4508f7367d9f09946c474393581b3bdd6c746033
Israel D. Matos & Hakim <pair+everysum1.hakimjoseph@devbootcamp.com>
2016-10-05 16:00:38 -0700

    Add tests to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(username: 'someDude', email: 'fakeaddress@fakedomain.com', password: 'password') }
+
+
+  it "has a username" do
+    expect(user.username).to eq('someDude')
+  end
+
+  it "has an email" do
+    expect(user.email).to eq('fakeaddress@fakedomain.com')
+  end
+
+  it "has an email that is unique" do
+    
+  end
+
+  it "has a password" do
+    expect(user.password).to exist
+  end
 end
