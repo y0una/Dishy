730f7db1e055790207d15671b09841a156a914e5
Israel D. Matos & Hakim <pair+everysum1.hakimjoseph@devbootcamp.com>
2016-10-05 19:20:46 -0700

    Add link to user show page in layout header

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
