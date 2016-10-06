acdff26c0b47da657b26eb1732be6d391e3f14f4
Kim Stephenson & Youna Yang <pair+kimstephenson.y0una@devbootcamp.com>
2016-10-06 11:44:24 -0700

    Refactor vote creation route

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,7 @@ class VotesController < ApplicationController
 
   def create
     @dish = Dish.find(params[:dish_id])
-    unless Vote.find_by(user_id: current_user.id, dish_id: @dish.id)
-      Vote.create(dish_id: @dish.id, user_id: current_user.id)
-    end
+    Vote.find_or_create_by(dish_id: @dish.id, user_id: current_user.id)
     redirect_to :back
   end
 end
