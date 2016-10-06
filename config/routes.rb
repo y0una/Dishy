9f754493711a55afb15c9ce08a5fc60d3c249ee0
Kim Stephenson & Youna Yang <pair+kimstephenson.y0una@devbootcamp.com>
2016-10-06 11:38:20 -0700

    Add route & partial for upvoting a dish

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :users, only: [:show, :edit, :update]
   resources :searches, only: [:show]
+  resources :dishes, only: [:index] do
+    resources :votes, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
