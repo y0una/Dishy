cf97ef81d8a0af861b49760b1a758eb4cd639e96
Hakim <email": null>
2016-10-06 16:05:15 -0700

    Create successful capybara test for registering a new user.

---
+++ spec/features/register_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+describe "the register process", :type => :feature do
+  # before :each do
+  #   User.make(email: 'user@example.com', password: 'password')
+  # end
+
+  it "registers a new user" do
+    visit '/users/sign_up'
+    within("#registerField") do
+      fill_in 'user[username]', with: 'Tester'
+      fill_in 'user[email]', with: 'Test@test.com'
+      fill_in 'user[password]', with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+    end
+    click_button 'Sign up'
+    expect(page).to have_content 'You have signed up successfully'
+  end
+end
