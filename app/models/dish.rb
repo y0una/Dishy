b32635430f975f200991f33e2cf715783547faea
Israel D. Matos & Hakim <pair+everysum1.hakimjoseph@devbootcamp.com>
2016-10-05 21:09:57 -0700

    Add initial search feature elements

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,12 @@
 class Dish < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  def self.search(search)
+    if search
+      find_by(:all, conditions: ['title LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
