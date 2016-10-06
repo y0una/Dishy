7862623e9a7e7c1b0bac8267514fbbc8a4975159
Israel D. Matos & Hakim <pair+everysum1.hakimjoseph@devbootcamp.com>
2016-10-06 10:59:24 -0700

    Implement search feature with seperate view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :users, only: [:show, :edit, :update]
-  get '/dishes/search' => "dishes#search"
+  resources :searches, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
