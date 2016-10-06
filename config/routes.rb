0ef96ff4a9be6966ff6819fcbd1ce79016748f8a
Israel D. Matos & Hakim <pair+everysum1.hakimjoseph@devbootcamp.com>
2016-10-05 18:48:05 -0700

    Fix routes to list root at top

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root "categories#index"
   resources :categories do
     resources :dishes
   end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "categories#index"
 end
