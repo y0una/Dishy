cdc4ddbfdd808fce67d8a72fe3539c1f1b41ddd9
Israel D. Matos & Hakim <pair+everysum1.hakimjoseph@devbootcamp.com>
2016-10-05 17:49:42 -0700

    Add user controller, views and helpers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  # before_action :authenticate_user!
+
+  def show
+    # redirect_to "/" unless current_user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :username, presence: true
+  has_many :dishes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
