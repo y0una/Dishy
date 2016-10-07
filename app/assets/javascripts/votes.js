ba5aa52a8ac8d4db56e3fe3298e2a3eac8b108ed
Kim Stephenson <email": null>
2016-10-07 16:48:45 -0700

    Begin ajaxing vote button color

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -14,6 +14,16 @@ var displayUpvotes = function() {
     });
     request.done(function(response) {
       $("#vote-count-" + response.id).html(response.text);
+      if($("#arugula-" + response.id).children().attr("src") == "/assets/arugula-green-8c08ec3c29d8d6dabab8b6271a8712b08fe4a66490733ac46c7986599c330c0c.png") {
+        $("#arugula-" + response.id).children().attr("src", "/assets/arugula.png");
+      } else {
+        $("#arugula-" + response.id).children().attr("src", "/assets/arugula-green.png");
+      }
+      // if($("#arugula-" + response.id).children().attr("src")) {
+      //   $("#arugula-" + response.id).children().attr("src", "/assets/arugula.png");
+      // } else {
+      //   $("#arugula-" + response.id).children().attr("src", "/assets/arugula-green.png");
+      // }
     });
   });
 }
